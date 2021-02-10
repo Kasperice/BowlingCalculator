c6abe779f1e23ca2b2af348b4850c05e1a82fc19
Kasper Biegun <kasper.biegun95@gmail.com>
2021-02-10 10:58:29 +0100

    Print string changed to fstrings

--- bowling_calculator.py
+++ bowling_calculator.py
@@ -8,7 +8,7 @@ Player = collections.namedtuple('Player', "name scores current_result")
 def prepare_lanes(number_of_lanes):
     lanes = []
     for i in range(number_of_lanes):
-        lanes.append("lane{}".format(i+1))
+        lanes.append(f"lane{i+1}")
     return lanes
 
 
@@ -18,7 +18,7 @@ number_of_lanes = 3
 lanes = prepare_lanes(number_of_lanes)
 for lane in lanes:
     try:
-        with open("{}.txt".format(lane)) as file:
+        with open(f"{lane}.txt") as file:
             players.clear()
             for line in file:
                 line = line.replace("\n", "")
@@ -27,7 +27,7 @@ for lane in lanes:
             print("*"*40)
             display_leaderboard(lane, players)
     except EnvironmentError:
-        print("{} is not used".format(lane))
+        print(f"{lane} is not used")
 
 
 
