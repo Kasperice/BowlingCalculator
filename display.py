8ab3d6c7a8a5007713a2358c5f1b85f35e2ac95e
Kasper Biegun <kasper.biegun95@gmail.com>
2021-02-10 10:58:29 +0100

    Print string changed to fstrings

--- display.py
+++ display.py
@@ -4,14 +4,14 @@ from counter import is_full_match
 
 def display_leaderboard(lane, players) -> None:
     players = sorted(players, key=attrgetter('current_result'), reverse=True)
-    print("{}".format(lane))
-    print("The current leader is:", getattr(players[0], 'name'), "\nLeaderboard:")
+    print("f{lane}")
+    print(f"The current leader is: {getattr(players[0], 'name')}\nLeaderboard:")
     for player in players:
         name, scores, result = unpack_tuple(player)
         print(name, scores, result)
 
     if is_full_match(scores):
-        print("\nGame is finished, congratulations", getattr(players[0], 'name'), "\n")
+        print(f"\nGame is finished, congratulations {getattr(players[0], 'name')}\n")
     else:
         print("\nGame is not finished yet, keep going!\n")
 
